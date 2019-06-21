c1593ba2aa91db1dbbf2ed60836c54bd82213484
devendra.kumar <devendra.kumar@bluestembrands.com>
2019-06-21 10:31:11 -0500

    Push the raw code

---
+++ src/main/java/com/msp/demo/mspconf/MspConfApplication.java
@@ -0,0 +1,13 @@
+package com.msp.demo.mspconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspConfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msp/demo/mspconf/MspConfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msp.demo.mspconf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MspConfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
